1e8acc439e1b9188f875c7033a39b5ac4573a322
unknown <bosszheng220@gmail.com>
2021-08-23 22:59:22 +0800

    Init commit

---
+++ src/utils/forecast.js
@@ -0,0 +1,18 @@
+const request = require('request')
+const chalk = require('chalk')
+
+const forecast = (latitude, longitude, callback) => {
+    const url = 'http://api.weatherstack.com/current?access_key=10313193af85c57e1d9af339bd091e0b&query=' + latitude + ',' + longitude
+    
+    request({url, json: true}, (error, {body}) => {
+        if(error) {
+            callback('Unable to connect to weather services.', undefined)
+        } else if (body.error) {
+            callback('Unable to find location', undefined)
+        } else {
+            callback(undefined, {forecastData: body.current.weather_descriptions[0] +'. Temperature: ' + body.current.temperature})
+        }
+    })
+}
+
+module.exports = forecast
